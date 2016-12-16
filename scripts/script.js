cbc6908b11094e1013e2c3856e9c571e8eaa11c6
DaffPunks <sawavas2@gmail.com>
2016-12-16 08:26:31 +0700

    (+) some sh1t 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,17 @@
-$(".button1").click(function () {
-    var text = $(".text1").val();
-    $(".message2").text(text);
-    $(".text1").val("");
-});
 
-$(".button2").click(function () {
-    var text = $(".text2").val();
-    $(".message1").text(text);
-    $(".text2").val("");
-});
+    //Код для первого окна
+    $(".button1").click(function () {
+        var text = $(".text1").val();
+        $(".message2").text(text);
+        $(".text1").val("");
+    });
+
+    //Код для второго окна
+    $(".button2").click(function () {
+        var text = $(".text2").val();
+        $(".message1").text(text);
+        $(".text2").val("");
+    });
+
+
+
